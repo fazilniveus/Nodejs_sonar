87a36a0259b3739afd36814714de927f31a5f2ea
fazilniveus <73817323+fazilniveus@users.noreply.github.com>
2022-12-05 10:50:23 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.93.74.17:9000/',
+  serverUrl: 'http://34.93.6.57:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'network',
